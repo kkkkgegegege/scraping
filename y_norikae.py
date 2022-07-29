10d323c55c8ac07c76ba1357fa9a08695d436bd7
kkkkgegegege <101812524+kkkkgegegege@users.noreply.github.com>
2022-07-29 16:28:25 +0900

    Add files via upload

--- y_norikae.py
+++ y_norikae.py
@@ -1,29 +1,44 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import WebDriverException
 import re
 import pandas as pd
 import time
 from tqdm import tqdm
 import sys
+import os
 
+filename = 'time_required.xlsx'
 URL = 'https://transit.yahoo.co.jp/'
-START_INPUT_TAG = '/html/body/div/body/div/div[1]/div[2]/div[1]/div[1]/div[2]/div[2]/form/dl[1]/dd/input'
-END_INPUT_TAG = '/html/body/div/body/div/div[1]/div[2]/div[1]/div[1]/div[2]/div[2]/form/dl[2]/dd/input'
-CITY_DF = pd.read_excel('city.xlsx')
+START_INPUT_TAG = '/html/body/div/div/div[2]/div[2]/div[1]/div[1]/div[2]/div[2]/form/dl[1]/dd/input'
+END_INPUT_TAG = '/html/body/div/div/div[2]/div[2]/div[1]/div[1]/div[2]/div[2]/form/dl[2]/dd/input'
+CITY_DF = pd.read_excel('適当\city.xlsx')
 START_CITIES = list(CITY_DF['検索用'])
-END_CITIES = ['東京駅', '大阪駅']
+END_CITIES = ['東京都庁', '大阪市役所', '名古屋市役所', '京都市役所', '横浜市役所', '神戸市役所', '北九州市役所', '札幌市役所'
+              , '川崎市役所', '福岡市役所', '広島市役所', '仙台市役所', '千葉市役所', 'さいたま市役所', '静岡市役所', '堺市役所'
+              , '新潟市役所', '浜松市役所', '岡山市役所', '相模原市役所', '熊本市役所']
 
-def firefox_driver():
-    options = Options()
-    options.headless = True
-    #user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"
-    #profile = webdriver.FirefoxProfile()
-    #profile.set_preference("general.useragent.override", user_agent)
-    driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver',options=options
-                                #,firefox_profile=profile
-                                )
-    return driver
+def is_file_exists(filename):
+    return os.path.isfile(filename)
+
+def get_last_data(filename):
+    df = pd.read_excel(filename)
+    last_data = df.tail(1)['start'].values[0]
+    return last_data
+
+def get_exsit_df(filename):
+    df = pd.read_excel(filename)
+    return df
+
+def save_data(new_data, filename):
+    if is_file_exists(filename):
+        exsited_data = get_exsit_df(filename)
+        df = pd.concat([new_data, exsited_data])
+        df.drop_duplicates(inplace = True)
+        df.to_excel(filename, index = False)
+    else:
+        new_data.to_excel(filename, index = False)
 
 def input_location(driver, start_or_end, location_name):
     if start_or_end == 'start':
@@ -75,18 +90,26 @@ def get_required_data(driver):
     return time_required, num_of_transfers, fee
 
 def main():
+    global START_CITIES
+    if is_file_exists(filename):
+        last_data = get_last_data(filename)
+        START_CITIES = START_CITIES[START_CITIES.index(last_data):]
     try:
         result = []
         for start_city in tqdm(START_CITIES):
             for end_city in END_CITIES:
-                firefox = firefox_driver()
-                firefox.get(URL)
-                input_location(firefox, 'start', start_city)
-                input_location(firefox, 'end', end_city)
-                check_no_time_specified(firefox)
-                click_search_button(firefox)
+                print(start_city, end_city)
+                options = webdriver.ChromeOptions()
+                options.add_argument('--headless')
+                options.add_argument("--no-sandbox")
+                Chorome = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+                Chorome.get(URL)
+                input_location(Chorome, 'start', start_city)
+                input_location(Chorome, 'end', end_city)
+                check_no_time_specified(Chorome)
+                click_search_button(Chorome)
                 try:
-                    time_required, num_of_transfers, fee = get_required_data(firefox)
+                    time_required, num_of_transfers, fee = get_required_data(Chorome)
                     result_dict = {
                         'start' : start_city
                         ,'end' : end_city
@@ -104,11 +127,17 @@ def main():
                     }
                     print(f'{start_city}:search failed')
                 result.append(result_dict)
-                firefox.quit()
+                Chorome.quit()
                 time.sleep(5)
-        pd.DataFrame(result).to_excel('time_required.xlsx')
+        new_data = pd.DataFrame(result)
+        save_data(new_data, filename)
+    except WebDriverException:
+        new_data = pd.DataFrame(result)
+        save_data(new_data, filename)
+        print('something went wrong')
     except KeyboardInterrupt:
-        pd.DataFrame(result).to_excel('time_required.xlsx')
+        new_data = pd.DataFrame(result)
+        save_data(new_data, filename)
         sys.exit('中断')
 if __name__ == '__main__':
     main()
